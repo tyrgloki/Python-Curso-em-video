56a53ec36f5e216c00cb3c023b23ad79179681fa
Loki <riqc0nt4ct@hotmail.com>
2022-06-18 13:59:25 -0300

    ex005-antecessor_sucessor

---
+++ ex005-antecessor_sucessor.py
@@ -0,0 +1,5 @@
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+num = int(input('Digite um número: '))
+
+print(f'{num}: antecessor {num-1} | sucessor {num+1}')
