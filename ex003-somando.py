8067e0e07a5f78551863e3b951cefedebdc0cf73
Loki <riqc0nt4ct@hotmail.com>
2022-06-18 13:24:52 -0300

    ex002-somando

---
+++ ex003-somando.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+print(f'{n1} + {n2} = {n1+n2}')
